85ddb7413f98859ad369968bd6c93e881d793221
Alan <aafloress>
2021-06-02 18:37:03 -0600

    Modelos en el admin

--- app_registro/views.py
+++ app_registro/views.py
@@ -3,6 +3,33 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+participantes = [
+    {
+        'nombre': 'Juan',
+        'apellido': 'Lopez',
+        'correo': 'juan@gmail.com',
+        'twitter': 'juan.lopez'
+    },
+    {
+        'nombre': 'Maria',
+        'apellido': 'Gomez',
+        'correo': 'maria@gmail.com',
+        'twitter': 'maria.gomez'
+    },
+    {
+        'nombre': 'Karla',
+        'apellido': 'Herrea',
+        'correo': 'karla.herrea@gmail.com',
+        'twitter': 'karla.herrera'
+    },
+    {
+        'nombre': 'Josue',
+        'apellido': 'Alvarez',
+        'correo': 'josue@gmail.com',
+        'twitter': 'josuetec2003'
+    },
+]
+
 def index(request):
 
     #clase del 24/05/2021
@@ -45,35 +72,30 @@ def index(request):
     #     'couta': c
     # }
 
-    participantes = [
-        {
-            'nombre': 'Juan',
-            'apellido': 'Lopez',
-            'correo': 'juan@gmail.com',
-            'twitter': 'juan.lopez'
-        },
-        {
-            'nombre': 'Maria',
-            'apellido': 'Gomez',
-            'correo': 'maria@gmail.com',
-            'twitter': 'maria.gomez'
-        },
-        {
-            'nombre': 'Karla',
-            'apellido': 'Herrea',
-            'correo': 'karla.herrea@gmail.com',
-            'twitter': 'karla.herrera'
-        },
-        {
-            'nombre': 'Josue',
-            'apellido': 'Alvarez',
-            'correo': 'josue@gmail.com',
-            'twitter': 'josuetec2003'
-        },
-    ]
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        correo = request.POST.get('correo')
+        twitter = request.POST.get('twitter')
 
-    ctx = {
-        'participantes': participantes
-    }
+        participantes.append({
+            'nombre': nombre,
+            'apellido': apellido,
+            'correo': correo,
+            'twitter': twitter
+        })
 
-    return render(request, 'registro/index.html', ctx)
+        ctx = {
+            'participantes': participantes
+        }
+
+        # return HttpResponse('El participante ha sido registrado')
+        return render(request, 'registro/index.html', ctx)
+    else:
+        # contexto que va para la plantilla
+        ctx = {
+            'participantes': participantes
+        }
+
+        return render(request, 'registro/index.html', ctx)
+
